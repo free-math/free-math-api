2393207d3300daac849e2bd5016448ffb8363310
yannunes <yannunes@gmail.com>
2017-05-18 08:39:17 -0300

    Adjusted import paths for new folder structure

--- api/main.js
+++ api/main.js
@@ -5,9 +5,9 @@ const async = require('async')
 
 const appDir = process.env.PWD + '/'
 
-const math = require('./math.api.js')
-const ocr = require('./ocr.api.js')
-const user = require('./user.api.js')
+const math = require('./math/math.api.js')
+const ocr = require('./ocr/ocr.api.js')
+const user = require('./user/user.api.js')
 
 module.exports = {
   math: math,
--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const path = require('path')
 const async = require('async')
 const appDir = process.env.PWD + '/'
 
-const db = require('./lib/mongoose.js')()
+const db = require('./lib/mongoose/mongoose.js')()
 module.exports.db = db
 
-const utils = require('./lib/utils.js')
+const utils = require('./lib/utils/utils.js')
 module.exports.utils = utils
 
-const math = require('./lib/math.js')
+const math = require('./lib/math/math.js')
 module.exports.math = math
 
-const ocr = require('./lib/ocr.js')
+const ocr = require('./lib/ocr/ocr.js')
 module.exports.ocr = ocr
 
-const user = require('./lib/user.js')
+const user = require('./lib/user/user.js')
 module.exports.user = user
 
 const api = require('./api/main.js')
